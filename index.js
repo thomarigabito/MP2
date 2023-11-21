5f1c8d248f1f54e6ab226d8c36faad2b33507930
thoamrigabito <tymagbito26@gmail.com>
2023-11-21 18:20:49 +0800

    modified:   index.html 	modified:   index.js 	modified:   style.css

---
+++ index.js
@@ -0,0 +1,19 @@
+    
+     
+let amount;
+let payment;
+let percent;
+let interest;
+    
+function myComputation() {
+    amount = Number(amount);
+    payment = Number(payment);
+    percent = Number(percent);
+    amount = document.getElementById("loanAmount").value;
+    payment = document.getElementById("loanPayment").value;
+    percent = document.getElementById("loanPercent").value;
+    let interest = amount * payment * percent
+   
+    interest = Number(interest)
+    document.getElementById("sampComp").innerHTML = "Principal & interest " + interest;
+}
