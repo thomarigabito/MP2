35daf32b459dff33c08d28078214450b3e2da0ad
thoamrigabito <tymagbito26@gmail.com>
2023-12-13 22:24:44 +0800

    new file:   Assets/house_specs/CamellaArielle.jpg 	new file:   Assets/house_specs/CamellaCriselle.jpg 	new file:   Assets/house_specs/CamellaDana.jpg 	new file:   Assets/house_specs/CamellaElla.jpg 	new file:   Assets/house_specs/CamellaEzabelle.jpg 	new file:   Assets/house_specs/CamellaFreya.jpg 	new file:   Assets/house_specs/CamellaGreta.jpg 	modified:   model.css 	modified:   model.html 	modified:   model.js 	new file:   package-lock.json 	new file:   package.json

--- model.js
+++ model.js
@@ -1,44 +1,54 @@
-      const product = [
+
+  
+
+const product = [
             {
                 id: 0,
-                image: 'Assets/compilehousemodels1/pexels-binyamin-mellish-106399.jpg',
+                image: 'Assets/house_specs/CamellaArielle.jpg',
                 title: 'Harmony Haven',
                 price: 1200000,
                 details: 'loginpage.html'
             },
             {
                 id: 1,
-                image: 'Assets/compilehousemodels1/pexels-binyamin-mellish-1396122.jpg',
+                image: 'Assets/house_specs/CamellaCriselle.jpg',
                 title: 'Serenity Suite',
                 price: 1500000,
                 details: 'index.html'
             },
             {
                 id: 2,
-                image: 'Assets/compilehousemodels1/pexels-binyamin-mellish-1396132.jpg',
+                image: 'Assets/house_specs/CamellaDana.jpg',
                 title: 'Crestview Cottage',
                 price: 2300000,
                 details: 'index.html'
             },
             {
                 id: 3,
-                image: 'Assets/compilehousemodels1/pexels-curtis-adams-3555615.jpg',
+                image: 'Assets/house_specs/CamellaElla.jpg',
                 title: 'Radiant Retreat',
                 price: 3500000,
                 details: 'index.html'
             },
             {
                 id: 4,
-                image: 'Assets/compilehousemodels1/pexels-curtis-adams-3990589.jpg',
+                image: 'Assets/house_specs/CamellaEzabelle.jpg',
                 title: 'Tranquil Terrace',
-                price: 2300000,
+                price: '2300000',
                 details: 'index.html'
             },
             {
                 id: 5,
-                image: 'Assets/compilehousemodels1/pexels-curtis-adams-5353946.jpg',
+                image: 'Assets/house_specs/CamellaFreya.jpg',
                 title: 'Majestic Manor',
-                price: 4500000,
+                price: '4500000',
+                details: 'index.html'
+            },
+            {
+                id: 6,
+                image: 'Assets/house_specs/CamellaGreta.jpg',
+                title: 'Majestic Manor',
+                price: '4500000',
                 details: 'index.html'
             },
             
@@ -59,6 +69,16 @@ const categories = [...new Set(product.map((item) => {
         })
         displayItem(filteredData)
     });
+    document.getElementById('searchBar').addEventListener('keyup', (e) => {
+        const searchData = e.target.value.toLowerCase();
+        const filteredData = categories.filter((item) => {
+            return (
+                item.price.toLowerCase().includes(searchData)
+            )
+
+        })
+        displayItem(filteredData)
+    });
     
     const displayItem = (items) => {
         document.getElementById("root").innerHTML = items.map((item) => {
@@ -68,17 +88,29 @@ const categories = [...new Set(product.map((item) => {
                     <div class='img-box'>
                         <img class='images' src=${image}></img>
                     </div> 
-                    <div class='bottom'>
-                        <p>${title}</p>
-                        <h2>PHP ${price}.00</h2>
-                    </div>
-                    <div class='btn-box'>
-                        <button><a href=${details}> Details </a> </button>
-                        <button> Book </button>
+                    <div class='detailsbox'>
+                        <div class='bottom'>
+                            <p>${title}</p>
+                            <h2>PHP ${price}.00</h2>
+                        </div>
+                        <div class='btn-box'>
+                            <button class="cardbtn"><a href=${details}> Details </a> </button>
+                            <button class="cardbtn"> Book </button>
+                        </div>
                     </div>
                 </div>`
                    
             )
         }).join('')
     };
-    displayItem(categories);
+displayItem(categories);
+
+
+
+
+
+
+
+
+    
+
