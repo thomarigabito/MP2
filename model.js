a2c462a52535f66f76787742a1f2d57b909daca4
JasperZnit <jaspersenit@gmail.com>
2023-12-18 10:09:57 +0800

    new file:   Assets/villaVideo.mp4 	modified:   model.css 	modified:   model.html 	modified:   model.js

--- model.js
+++ model.js
@@ -1,90 +1,202 @@
-
-  
-
 const product = [
-            {
-                id: 0,
-                image: 'Assets/house_specs/CamellaArielle.jpg',
-                title: 'Harmony Haven',
-                price: 1200000,
-                details: 'loginpage.html'
-            },
-            {
-                id: 1,
-                image: 'Assets/house_specs/CamellaCriselle.jpg',
-                title: 'Serenity Suite',
-                price: 1500000,
-                details: 'index.html'
-            },
-            {
-                id: 2,
-                image: 'Assets/house_specs/CamellaDana.jpg',
-                title: 'Crestview Cottage',
-                price: 2300000,
-                details: 'index.html'
-            },
-            {
-                id: 3,
-                image: 'Assets/house_specs/CamellaElla.jpg',
-                title: 'Radiant Retreat',
-                price: 3500000,
-                details: 'index.html'
-            },
-            {
-                id: 4,
-                image: 'Assets/house_specs/CamellaEzabelle.jpg',
-                title: 'Tranquil Terrace',
-                price: '2300000',
-                details: 'index.html'
-            },
-            {
-                id: 5,
-                image: 'Assets/house_specs/CamellaFreya.jpg',
-                title: 'Majestic Manor',
-                price: '4500000',
-                details: 'index.html'
-            },
-            {
-                id: 6,
-                image: 'Assets/house_specs/CamellaGreta.jpg',
-                title: 'Majestic Manor',
-                price: '4500000',
-                details: 'index.html'
-            },
-            
+  {
+    id: 0,
+    image: "Assets/house_specs/CamellaArielle.jpg",
+    title: "Harmony Haven",
+    price: 1200000,
+    details: "loginpage.html",
+  },
+  {
+    id: 1,
+    image: "Assets/house_specs/CamellaCriselle.jpg",
+    title: "Serenity Suite",
+    price: 1500000,
+    details: "index.html",
+  },
+  {
+    id: 2,
+    image: "Assets/house_specs/CamellaDana.jpg",
+    title: "Crestview Cottage",
+    price: 2300000,
+    details: "index.html",
+  },
+  {
+    id: 3,
+    image: "Assets/house_specs/CamellaElla.jpg",
+    title: "Radiant Retreat",
+    price: 3500000,
+    details: "index.html",
+  },
+  {
+    id: 4,
+    image: "Assets/house_specs/CamellaEzabelle.jpg",
+    title: "Tranquil Terrace",
+    price: "2300000",
+    details: "index.html",
+  },
+  {
+    id: 5,
+    image: "Assets/house_specs/CamellaFreya.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 6,
+    image: "Assets/house_specs/CamellaGreta.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 7,
+    image: "Assets/house_specs/CamellaGreta.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 4,
+    image: "Assets/house_specs/CamellaEzabelle.jpg",
+    title: "Tranquil Terrace",
+    price: "2300000",
+    details: "index.html",
+  },
+  {
+    id: 5,
+    image: "Assets/house_specs/CamellaFreya.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 6,
+    image: "Assets/house_specs/CamellaGreta.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 7,
+    image: "Assets/house_specs/CamellaGreta.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 4,
+    image: "Assets/house_specs/CamellaEzabelle.jpg",
+    title: "Tranquil Terrace",
+    price: "2300000",
+    details: "index.html",
+  },
+  {
+    id: 5,
+    image: "Assets/house_specs/CamellaFreya.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 6,
+    image: "Assets/house_specs/CamellaGreta.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 7,
+    image: "Assets/house_specs/CamellaGreta.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 4,
+    image: "Assets/house_specs/CamellaEzabelle.jpg",
+    title: "Tranquil Terrace",
+    price: "2300000",
+    details: "index.html",
+  },
+  {
+    id: 5,
+    image: "Assets/house_specs/CamellaFreya.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 6,
+    image: "Assets/house_specs/CamellaGreta.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 7,
+    image: "Assets/house_specs/CamellaGreta.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 4,
+    image: "Assets/house_specs/CamellaEzabelle.jpg",
+    title: "Tranquil Terrace",
+    price: "2300000",
+    details: "index.html",
+  },
+  {
+    id: 5,
+    image: "Assets/house_specs/CamellaFreya.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 6,
+    image: "Assets/house_specs/CamellaGreta.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+  {
+    id: 7,
+    image: "Assets/house_specs/CamellaGreta.jpg",
+    title: "Majestic Manor",
+    price: "4500000",
+    details: "index.html",
+  },
+];
 
-            
-]
-        
-const categories = [...new Set(product.map((item) => {
-        return item}))]
+const categories = [
+  ...new Set(
+    product.map((item) => {
+      return item;
+    })
+  ),
+];
 
-    document.getElementById('searchBar').addEventListener('keyup', (e) => {
-        const searchData = e.target.value.toLowerCase();
-        const filteredData = categories.filter((item) => {
-            return (
-                item.title.toLowerCase().includes(searchData)
-            )
+document.getElementById("searchBar").addEventListener("keyup", (e) => {
+  const searchData = e.target.value.toLowerCase();
+  const filteredData = categories.filter((item) => {
+    return item.title.toLowerCase().includes(searchData);
+  });
+  displayItem(filteredData);
+});
+document.getElementById("searchBar").addEventListener("keyup", (e) => {
+  const searchData = e.target.value.toLowerCase();
+  const filteredData = categories.filter((item) => {
+    return item.price.toLowerCase().includes(searchData);
+  });
+  displayItem(filteredData);
+});
 
-        })
-        displayItem(filteredData)
-    });
-    document.getElementById('searchBar').addEventListener('keyup', (e) => {
-        const searchData = e.target.value.toLowerCase();
-        const filteredData = categories.filter((item) => {
-            return (
-                item.price.toLowerCase().includes(searchData)
-            )
-
-        })
-        displayItem(filteredData)
-    });
-    
-    const displayItem = (items) => {
-        document.getElementById("root").innerHTML = items.map((item) => {
-            var { image, title, price, details,} = item;
-            return (
-                `<div class='box'>
+const displayItem = (items) => {
+  document.getElementById("root").innerHTML = items
+    .map((item) => {
+      var { image, title, price, details } = item;
+      return `<div class='box'>
                     <div class='img-box'>
                         <img class='images' src=${image}></img>
                     </div> 
@@ -98,19 +210,8 @@ const categories = [...new Set(product.map((item) => {
                             <button class="cardbtn"> Book </button>
                         </div>
                     </div>
-                </div>`
-                   
-            )
-        }).join('')
-    };
+                </div>`;
+    })
+    .join("");
+};
 displayItem(categories);
-
-
-
-
-
-
-
-
-    
-
